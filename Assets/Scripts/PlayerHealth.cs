f1cc929da80a6766f32f7bb1d302593d3a73f000
Tomasque <darklorddwarf@gmail.com>
2024-07-20 22:02:43 -0700

    Health Bar & Death Zone

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -2,19 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [SerializeField] List<GameObject> HP;
+    int currentHP;
+
+    private void Awake()
+    {
+        currentHP = HP.Count;
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        print("a");
-
         Tags T = collision.gameObject.GetComponent<Tags>();
-        if (T == null || !T.harms)
+        if (T == null || !(T.harms || T.instantDeath))
             return;
 
-        print("b");
+        currentHP--;
+        HP[currentHP].GetComponent<Image>().color = new Color(.5f, .5f, .5f, HP[currentHP].GetComponent<Image>().color.a);
+        HP[currentHP].transform.GetChild(0).gameObject.SetActive(false);
 
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        if (currentHP == 0 || T.instantDeath)
+        {
+            foreach(GameObject g in HP)
+            {
+                g.GetComponent<Image>().color = new Color(.5f, .5f, .5f, g.GetComponent<Image>().color.a);
+                if(g.transform.childCount > 0) g.transform.GetChild(0).gameObject.SetActive(false);
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+
     }
 }
--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -8,4 +8,5 @@ public class Tags : MonoBehaviour
     public bool blocksBullets;
     public bool harms;
     public bool patrolBorder;
+    public bool instantDeath;
 }
